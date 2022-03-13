fd11af9d1e7a55cb01e5bd65e57d040623f4f5d5
Alina Konkova <99170615+AlinaKonkova@users.noreply.github.com>
2022-03-13 20:53:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,35 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int lg = 0;
+  int rg = size - 1;
+  int count = 0;
+  int check = -1;
+  while (lg <= rg) {
+    int mid = (lg + rg) / 2;
+    if (arr[mid] == value) {
+     count++;
+      check = mid;
+      break;
+     }
+     if (value < arr[mid])
+       rg = mid - 1;
+     else
+       lg = mid + 1;
+  }
+  if (check >= 0) {
+    for (int st = 0; st < check; st++) {
+      if (arr[st] == value)
+        count++;
+    }
+    for (int end = size - 1; end > check; end--) {
+      if (arr[end] == value)
+        count++;
+    }
+  }
+  if (count > 0) {
+    return count;
+  }
+  else 
+    return 0; 
 }
